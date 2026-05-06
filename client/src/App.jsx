36182f5332c7afc55a8f806a52c7d4d0fd5cbd80
Ahmed Isam <ahmdsamth@gmail.com>
2026-05-06 17:46:45 +0400

    Update SERVER_URL to point to production server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home.jsx'
 import Lobby from './pages/Lobby.jsx'
 import Game from './pages/Game.jsx'
 import Results from './pages/Results.jsx'
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001'
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://ahmed.hackclub.app'
 
 
 export default function App() {
