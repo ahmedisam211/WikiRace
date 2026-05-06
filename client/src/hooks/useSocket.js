7fbe84ede48e48f9f7cf745187f51eb97a04ad0f
Ahmed Isam <ahmdsamth@gmail.com>
2026-05-06 17:49:05 +0400

    Update SERVER_URL to remove port number

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 import { io } from 'socket.io-client'
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://ahmed.hackclub.app:3001'
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://ahmed.hackclub.app'
 
 export function useSocket() {
   const socketRef = useRef(null)
